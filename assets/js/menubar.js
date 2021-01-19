f2c29d91116ac3105607873e9e578e231184a9ad
Raihan Kabir <raihancse46@gmail.com>
2021-01-19 16:36:02 +0600

    add main template

---
+++ assets/js/menubar.js
@@ -0,0 +1,36 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "280px";
+    //document.getElementById("dark-bg").style.display = 'block';
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    //document.getElementById("dark-bg").style.display = 'none';
+  }
+  function openNav2() {
+    document.getElementById("mySidenav-2").style.width = "280px";
+    //document.getElementById("dark-bg").style.display = 'block';
+  }
+  
+  function closeNav2() {
+    document.getElementById("mySidenav-2").style.width = "0";
+    //document.getElementById("dark-bg").style.display = 'none';
+  }
+
+  //header search click to expand 
+  $(document).ready(function () {
+    $(".cs-btn-submit").click(function() {
+      $(".cs-txt-livesearch").addClass("sopen");
+      $(".cs-btn-close").addClass("cs-show");
+      $(".cs-btn-submit").addClass("cs-hide");
+    })
+
+    $(".cs-btn-close").click(function() {
+      $(".cs-txt-livesearch").removeClass("sopen");
+      $(".cs-btn-close").removeClass("cs-show");
+      $(".cs-btn-submit").removeClass("cs-hide");
+    })
+  })
+
+
